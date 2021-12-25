5ba69874536ec23383a6d80a903b4f3a333bb731
Rakave Kandiah <88566790+Rak97@users.noreply.github.com>
2021-12-24 22:29:02 -0500

    console log daily weather forecast

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
    function displayForecast() {
-
+        console.log(response.data.daily);
         let forecastElement = document.querySelector("#forecast"); 
 
         let forecastHTML = `<div class="row">`;
@@ -19,6 +19,13 @@
         forecastElement.innerHTML = forecastHTML;
 
      } 
+
+  function getForecast(coordinates) {
+       console.log(coordinates);
+       let apiKey = "4603cd08f9aa4435f5a1a0fde738051c";
+       let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+       axios.get(apiUrl).then(displayForecast);
+     }
 
      function displayTemperature(response) {
         let descripitionElement = document.querySelector("#descripition");
@@ -40,7 +47,8 @@
           "src",
           `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
         );
-
+        
+        getForecast(response.data.coord);
         //Date 
 
         let currentDate = new Date();
@@ -123,9 +131,6 @@
 
       let searchGeo = document.querySelector("#liveLocate");
       searchGeo.addEventListener("click", handleGeo);
-
-      // Displaying Forecast 
-        displayForecast();
 
       // Display City 
         search("Yellowknife");
