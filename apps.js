4573ee9502b5fe5fd5c084d2d395d221d2171ece
Rakave Kandiah <88566790+Rak97@users.noreply.github.com>
2021-12-24 22:59:11 -0500

    rounding up temperature (max & min)

--- apps.js
+++ apps.js
@@ -18,7 +18,7 @@ function displayForecast(response) {
           <div class="col-2">
             <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
               <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png" alt="" width="50">
-        <span class="weather-forecast-temp-max">${forecastDay.temp.max}°</span><span class="weather-forecast-temp-min"> ${forecastDay.temp.min}°</span>
+        <span class="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span><span class="weather-forecast-temp-min"> ${Math.round(forecastDay.temp.min)}°</span>
         </div>`;
         }) 
 
